998c17298bda6573f4a75c6487b95680b88e26f5
vmhoang1703 <vmhoang1703@gmail.com>
2024-07-06 13:35:54 +0700

    Create Microservices Clients and Configure Gateway Module

--- backend-microservices/todo-gateway/src/app.module.ts
+++ backend-microservices/todo-gateway/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'TASKS_SERVICE',
+        transport: Transport.TCP,
+        options: { host: '127.0.0.1', port: 3001 },
+      },
+      {
+        name: 'USERS_SERVICE',
+        transport: Transport.TCP,
+        options: { host: '127.0.0.1', port: 3002 },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
