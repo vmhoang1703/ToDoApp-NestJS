316ce5c67a6c1e7dfb14eb1fcb128822c9a19f15
vmhoang1703 <vmhoang1703@gmail.com>
2024-07-06 13:44:46 +0700

    Create Gateway Controller and Service

--- backend-microservices/todo-gateway/src/app.controller.ts
+++ backend-microservices/todo-gateway/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('tasks')
+  createTask(@Body() taskDto: any) {
+    return this.appService.createTask(taskDto);
+  }
+
+  @Get('tasks')
+  findAllTasks() {
+    return this.appService.findAllTasks();
+  }
+
+  @Post('users')
+  createUser(@Body() userDto: any) {
+    return this.appService.createUser(userDto);
+  }
+
+  @Get('users')
+  findAllUsers() {
+    return this.appService.findAllUsers();
   }
 }
--- backend-microservices/todo-gateway/src/app.service.ts
+++ backend-microservices/todo-gateway/src/app.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject('TASKS_SERVICES') private readonly tasksClient: ClientProxy,
+    @Inject('USERS_SERVICE') private readonly usersClient: ClientProxy,
+  ) {}
+
+  createTask(taskDto: any) {
+    return this.tasksClient.send('createTask', taskDto);
+  }
+
+  findAllTasks() {
+    return this.tasksClient.send('findAllTasks', {});
+  }
+
+  createUser(userDto: any) {
+    return this.usersClient.send('createUser', userDto);
+  }
+
+  findAllUsers() {
+    return this.usersClient.send('findAllUsers', {});
   }
 }
