9e94e94d4335ee70a32352d297280de892c44ebb
vmhoang1703 <vmhoang1703@gmail.com>
2024-07-09 08:02:51 +0700

    make jwt service for creating access token for user and make AuthGuard for proctecting route

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/Todo-NestJS'),
     TasksModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './schemas/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async findOne(username: string): Promise<any> {
+    return await this.userModel.findOne({ username }).exec();
+  }
+}
